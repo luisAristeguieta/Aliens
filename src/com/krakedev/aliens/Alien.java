5714f2bf497a62eefc75e3dfe4324a31e0dc4a15
luisjimenezgx@gmai.com <luisjimenezgx@gmail.com>
2026-04-18 13:32:16 -0400

    feat(Alien): add getters and imprimir method

--- src/com/krakedev/aliens/Alien.java
+++ src/com/krakedev/aliens/Alien.java
@@ -9,10 +9,9 @@ public class Alien {
 	private double precioExtremidad;
 	private double precioOjo;
 	private double precioCuerpo;
-	
-	
+
 	public Alien(int tamanio, String color) {
-		
+
 		if (tamanio < 5) {
 			this.tamanio = 5;
 		} else if (tamanio > 30) {
@@ -27,5 +26,45 @@ public class Alien {
 		this.precioExtremidad = this.tamanio * 0.10;
 		this.precioOjo = this.tamanio * 0.05;
 	}
-	
+
+	public int getTamanio() {
+		return tamanio;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public int getNumeroOjos() {
+		return numeroOjos;
+	}
+
+	public int getNumeroBrazos() {
+		return numeroBrazos;
+	}
+
+	public int getNumeroPies() {
+		return numeroPies;
+	}
+
+	public double getPrecioExtremidad() {
+		return precioExtremidad;
+	}
+
+	public double getPrecioOjo() {
+		return precioOjo;
+	}
+
+	public double getPrecioCuerpo() {
+		return precioCuerpo;
+	}
+
+	public void imprimir() {
+		String mensaje = "Tamaño: " + tamanio + " cm" + " , Color: " + color + " , Numero de Ojos: " + numeroOjos
+				+ " , Numero de Brazos: " + numeroBrazos + " , Numero de Pies: " + numeroPies
+				+ " , Precio Extremidad(es): " + precioExtremidad + "$" + " , Precio Ojo(s): " + precioOjo + "$"
+				+ " , Precio Cuerpo: " + precioCuerpo + "$";
+		System.out.println(mensaje);
+	}
+
 }
