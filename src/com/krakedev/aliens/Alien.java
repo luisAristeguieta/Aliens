83cb37beb6c4df21bfbe2ef14b03badca331dabf
luisjimenezgx@gmai.com <luisjimenezgx@gmail.com>
2026-04-18 13:16:15 -0400

    feat(Alien): add attributes

--- src/com/krakedev/aliens/Alien.java
+++ src/com/krakedev/aliens/Alien.java
@@ -1,5 +1,13 @@
 package com.krakedev.aliens;
 
 public class Alien {
-
+	private int tamanio;
+	private String color;
+	private int numeroOjos;
+	private int numeroBrazos;
+	private int numeroPies;
+	private double precioExtremidad;
+	private double precioOjo;
+	private double precioCuerpo;
+	
 }
