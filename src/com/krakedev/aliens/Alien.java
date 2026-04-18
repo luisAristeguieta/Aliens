c39b3c6d215c2ff95c70cb7f447cdf9b633427a3
luisjimenezgx@gmai.com <luisjimenezgx@gmail.com>
2026-04-18 13:21:54 -0400

    feat(Alien): add constructor

--- src/com/krakedev/aliens/Alien.java
+++ src/com/krakedev/aliens/Alien.java
@@ -10,4 +10,22 @@ public class Alien {
 	private double precioOjo;
 	private double precioCuerpo;
 	
+	
+	public Alien(int tamanio, String color) {
+		
+		if (tamanio < 5) {
+			this.tamanio = 5;
+		} else if (tamanio > 30) {
+			this.tamanio = 30;
+		} else {
+			this.tamanio = tamanio;
+		}
+
+		this.color = color;
+
+		this.precioCuerpo = this.tamanio * 0.20;
+		this.precioExtremidad = this.tamanio * 0.10;
+		this.precioOjo = this.tamanio * 0.05;
+	}
+	
 }
